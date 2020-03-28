5321a77e5f944a21a11b81b29c55785a610355c2
Rodrigo Botelho <tato.rodrigo@gmail.com>
2020-03-28 18:00:54 -0300

    testes unitários e de integração

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,11 +1,21 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './src/database/db.sqlite'
+      filename: "./src/database/db.sqlite"
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/db.test.sqlite"
     },
     migrations: {
       directory: "./src/database/migrations"
@@ -14,35 +24,34 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
